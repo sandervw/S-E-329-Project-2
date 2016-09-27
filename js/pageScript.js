be34c3259b1ef0a520e69bf70d454a862a065c77
zvlovisa <zvlovisa@iastate.edu>
2016-09-27 14:58:57 -0500

    Did some cleaning up, and added prompts for the log in and sign up buttons. Still need validation and actual functionality for these buttons

---
+++ js/pageScript.js
@@ -0,0 +1,19 @@
+//Function To Display Sign Up Popup
+function signUp_show() {
+  document.getElementById('signUp').style.display = "block";
+}
+
+//Function to Hide Sign Up Popup
+function signUp_hide(){
+  document.getElementById('signUp').style.display = "none";
+}
+
+//Function To Display Log In Popup
+function logIn_show() {
+  document.getElementById('logIn').style.display = "block";
+}
+
+//Function to Hide Log In Popup
+function logIn_hide(){
+  document.getElementById('logIn').style.display = "none";
+}
